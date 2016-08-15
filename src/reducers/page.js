ab037d157e69d7a0b7b4ca27a03517ac003e465c
Valerijpl <valerijpll@gmail.com>
2016-08-15 04:34:58 +0300

    Change file

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,18 +1,37 @@
-import { SET_YEAR } from '../constants/Page'
+import { GET_COMMUNITY_REQUEST} from '../constants/Page'
+import { GET_COMMUNITY_SUCCESS} from '../constants/Page'
 
 const initialState = {
-  year: 2016,
-  photos: []
+  response : null
 }
 
 export default function page(state = initialState, action) {
 
   switch (action.type) {
-    case SET_YEAR:          
-      return { ...state, year: action.payload }
+    case GET_COMMUNITY_SUCCESS:
+    return { ...response:action.payload }
 
     default:
-      return state;
-  }
+    return response;
 
+  }
 }
+
+// import { SET_YEAR } from '../constants/Page'
+//
+// const initialState = {
+//   year: 2016,
+//   photos: []
+// }
+//
+// export default function page(state = initialState, action) {
+//
+//   switch (action.type) {
+//     case SET_YEAR:
+//       return { ...state, year: action.payload }
+//
+//     default:
+//       return state;
+//   }
+//
+// }
