59ee70298b2e45063deb0288a1384168c7768d56
Valerijpl <valerijpll@gmail.com>
2016-08-15 04:31:50 +0300

    Change file

--- src/actions/PageActions.js
+++ src/actions/PageActions.js
@@ -1,10 +1,27 @@
-import { SET_YEAR } from '../constants/Page'
+import { GET_COMMUNITY_REQUEST } from '../constants/Page'
+import { GET_COMMUNITY_SUCCESS } from '../constants/Page'
 
-export function setYear(year) {
-
-  return {
-    type: SET_YEAR, 
-    payload: year
-  }
-
+export function getCommunityRequest(){
+return {
+  type: GET_COMMUNITY_SUCCESS,
+  payload: response
+}  
 }
+
+fetch(url)
+.then(data=>data.json)
+.then(response=>{console.log(response)})
+
+
+
+// import { SET_YEAR } from '../constants/Page'
+//
+// export function setYear(year) {
+//
+//   return {
+//     type: SET_YEAR,
+//     payload: year
+//   }
+//
+// }
+
