7a232884b95a99cf6cb6139759dc808c0452cbf3
Valerijpl <valerijpll@gmail.com>
2016-08-29 19:16:07 +0300

    Update page.js

--- src/reducers/page.js
+++ src/reducers/page.js
@@ -1,37 +1,37 @@
-import { GET_COMMUNITY_REQUEST} from '../constants/Page'
-import { GET_COMMUNITY_SUCCESS} from '../constants/Page'
+// import { GET_COMMUNITY_REQUEST} from '../constants/Page'
+// import { GET_COMMUNITY_SUCCESS} from '../constants/Page'
+
+// const initialState = {
+//   response : null
+// }
+
+// export default function page(state = initialState, action) {
+
+//   switch (action.type) {
+//     case GET_COMMUNITY_SUCCESS:
+//     return { ...response:action.payload }
+
+//     default:
+//     return response;
+
+//   }
+// }
+
+import { SET_YEAR } from '../constants/Page'
 
 const initialState = {
-  response : null
+  year: 2016,
+  photos: []
 }
 
 export default function page(state = initialState, action) {
 
   switch (action.type) {
-    case GET_COMMUNITY_SUCCESS:
-    return { ...response:action.payload }
+    case SET_YEAR:
+      return { ...state, year: action.payload }
 
     default:
-    return response;
-
+      return state;
   }
-}
 
-// import { SET_YEAR } from '../constants/Page'
-//
-// const initialState = {
-//   year: 2016,
-//   photos: []
-// }
-//
-// export default function page(state = initialState, action) {
-//
-//   switch (action.type) {
-//     case SET_YEAR:
-//       return { ...state, year: action.payload }
-//
-//     default:
-//       return state;
-//   }
-//
-// }
+}
